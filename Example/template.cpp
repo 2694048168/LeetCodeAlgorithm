0a7bba2ca91848d8340edd6bba6da9c10542571a
云主宰苍穹 <2694048168@qq.com>
2020-09-22 21:56:52 +0800

    add example

---
+++ Example/template.cpp
@@ -0,0 +1,35 @@
+/**
+ * LeetCodeLink ： https://leetcode-cn.com/problems/regular-expression-matching
+ * 
+ * 10. 正则表达式匹配
+ * 
+ */
+
+//![include]
+#include <iostream>
+
+using namespace std;
+//![include]
+
+//![solution]
+
+/**
+ * 
+ */
+class Solution 
+{
+public:
+
+};
+//![solution]
+
+/**
+ * @function Main function
+ */
+int main(int argc, char **argv)
+{
+    // [test]
+
+    return 0;
+}
+
---
+++ Example/template.py
@@ -0,0 +1,17 @@
+#
+# LeetCodeLink ： https://leetcode-cn.com/problems/regular-expression-matching
+#
+# 10. 正则表达式匹配
+#
+
+class Solution:
+    """
+
+    """
+    def FunctionName(args):
+        
+
+
+
+if __name__ == "__main__":
+    pass
